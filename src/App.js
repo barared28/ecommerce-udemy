ae5dd6ac791179ca15055f43451cd40deaf46f30
Bara Borneo <bara@majoo.id>
2022-01-10 16:17:08 +0700

    push all together task week 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import {Route, Switch} from "react-router-dom";
 import './App.css';
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./component/header/header.componet";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const HatsPage = () => (
+    <div>
+        <h1>HATS PAGE</h1>
     </div>
-  );
+);
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUser: null,
+        }
+    }
+
+    unsubscribeFromAuth = null;
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged( async user => {
+            if (user) {
+                const userRef = await createUserProfileDocument(user);
+                userRef.onSnapshot(snapshot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapshot.id,
+                            ...snapshot.data(),
+                        }
+                    })
+                });
+            }
+            this.setState({ currentUser: user });
+        });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
+    render() {
+        const { currentUser } = this.state;
+        return (
+          <div>
+              <Header currentUser={currentUser} />
+              <Switch>
+                  <Route exact path='/' component={HomePage} />
+                  <Route path='/shop' component={ShopPage} />
+                  <Route path='/hats' component={HatsPage} />
+                  <Route path='/signin' component={SignInAndSignUpPage} />
+              </Switch>
+          </div>
+      );
+  }
 }
 
 export default App;
